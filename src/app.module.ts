9d6f284b7405b4ad05a37adc88d3ae726fc93f90
TorlettiJoaquin <axeltorletti@gmail.com>
2025-12-30 13:35:56 -0300

    feat: add Redis caching and update event handlers in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,13 @@ import { ConfigModule } from '@nestjs/config';
 import { CqrsModule } from '@nestjs/cqrs';
 
 import { ClubUpdatedHandler } from './domain/handlers/club-updated.handler';
+import { CourtUpdatedHandler } from './domain/handlers/court-updated.handler';
 import { GetAvailabilityHandler } from './domain/handlers/get-availability.handler';
+import { SlotAvailableHandler } from './domain/handlers/slot-available.handler';
+import { SlotBookedHandler } from './domain/handlers/slot-booked.handler';
 import { ALQUILA_TU_CANCHA_CLIENT } from './domain/ports/aquila-tu-cancha.client';
+import { RedisCacheService } from './infrastructure/cache/redis-cache.service';
+import { CachedAlquilaTuCanchaClient } from './infrastructure/clients/cached-alquila-tu-cancha.client';
 import { HTTPAlquilaTuCanchaClient } from './infrastructure/clients/http-alquila-tu-cancha.client';
 import { EventsController } from './infrastructure/controllers/events.controller';
 import { SearchController } from './infrastructure/controllers/search.controller';
@@ -14,12 +19,17 @@ import { SearchController } from './infrastructure/controllers/search.controller
   imports: [HttpModule, CqrsModule, ConfigModule.forRoot()],
   controllers: [SearchController, EventsController],
   providers: [
+    RedisCacheService,
+    HTTPAlquilaTuCanchaClient,
     {
       provide: ALQUILA_TU_CANCHA_CLIENT,
-      useClass: HTTPAlquilaTuCanchaClient,
+      useClass: CachedAlquilaTuCanchaClient,
     },
     GetAvailabilityHandler,
     ClubUpdatedHandler,
+    CourtUpdatedHandler,
+    SlotBookedHandler,
+    SlotAvailableHandler,
   ],
 })
 export class AppModule {}
